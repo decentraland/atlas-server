6b3256478536d0878bfd842e9bc59f620f5a81b6
Ignacio Mazzara <nachomazzara@gmail.com>
2021-05-11 17:20:19 -0300

    refactor: use seconds

--- src/modules/api/component.ts
+++ src/modules/api/component.ts
@@ -334,7 +334,7 @@ export async function createApiComponent(components: {
 
     if (activeOrder && !isExpired(activeOrder)) {
       tile.price = Math.round(parseInt(activeOrder.price) / 1e18)
-      tile.expiresAt = parseInt(activeOrder.expiresAt, 10)
+      tile.expiresAt = Math.round(parseInt(activeOrder.expiresAt, 10) / 1000)
     }
 
     if (tokenId) {
