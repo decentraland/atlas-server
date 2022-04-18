866b5c46343ba0ba790a715c43d61fad3da4f8ef
Juan Cazala <juancazala@gmail.com>
2022-04-18 15:39:29 -0300

    feat: added 404 estate image

--- src/controllers/handlers.ts
+++ src/controllers/handlers.ts
@@ -148,6 +148,16 @@ export const estateMapPngRequestHandler = async (context: {
     const selected = Object.values(tiles).filter(
       (tile) => tile.estateId && tile.estateId === estateId
     )
+    if (selected.length === 0) {
+      const  headers: Record<string, string> = {
+        'content-type': 'image/png',
+        'location': 'https://ui.decentraland.org/dissolved_estate.png'
+      }
+      return {
+        status: 301,
+        headers
+      }
+    }
     const xs = selected.map((coords) => coords.x).sort()
     const ys = selected.map((coords) => coords.y).sort()
     const x = xs[(xs.length / 2) | 0] || 0
