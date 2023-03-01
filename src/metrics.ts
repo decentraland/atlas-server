565357161e0a265a89bd639b9a0786be436e2fd0
Lautaro Petaccio <lausuper@gmail.com>
2023-03-01 12:52:41 -0500

    fix: Fix metric help

--- src/metrics.ts
+++ src/metrics.ts
@@ -24,7 +24,7 @@ export const metricDeclarations = {
   },
   dcl_map_update: {
     type: IMetricsComponent.CounterType,
-    help: 'Update failures',
+    help: 'Updates',
     labelNames: ['type', 'status'],
   },
 }
